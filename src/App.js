ba3a773d91d12054fab0249d2ad43f950d94f5de
Panchakshari Hugar, Udaya <upanchaksharihuga@deloitte.com>
2020-04-19 22:26:16 +0530

    #54 handling dynamic content javascript way.

--- src/App.js
+++ src/App.js
@@ -1,116 +1,104 @@
-import React,{Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person' 
-import UserInput from './User/UserInput'
-import UserOutput from './User/UserOutput';
+import Person from './Person/Person'
+// import UserInput from './User/UserInput'
+// import UserOutput from './User/UserOutput';
 
 class App extends Component {
-  // use state hook provides state storage for functional components.
-  // for class component, it will replace only the part of the object is need to be updated. 
+  // use state hook provides state storage for functional components. for class component, it will replace only the part of the object is need to be updated. 
   // example, it will replace persons if that is sub-object is passed.
-  // for functional component, it will replace whole object. 
-  // example, it will replace persons with new object. also, otherdata object is lost.
+  // for functional component, it will replace whole object. example, it will replace persons with new object. also, otherdata object is lost.
   state = {
-    persons:[
-      {name:'Uday', age:29, hobbies:'My hobbies are cooking, listening to music'},
-      {name:'Vijay', age:28},
-      {name:'Vinay', age:19 }
+    persons: [
+      { name: 'Uday', age: 29, hobbies: 'My hobbies are cooking, listening to music' },
+      { name: 'Vijay', age: 28 },
+      { name: 'Vinay', age: 19 }
     ],
-    users :[
-      {username:'udayh', email:'uday@test.com', name:'Uday'},
-      {username:'vijaykumar', email:'vijay@test.com', name:'Vijay'}
+    users: [
+      { username: 'udayh', email: 'uday@test.com', name: 'Uday' },
+      { username: 'vijaykumar', email: 'vijay@test.com', name: 'Vijay' }
     ]
   }
 
-  switchNameHandler =(newName)=>{
+  switchNameHandler = (newName) => {
     this.setState({
-      persons:[
-        {name:newName, age:29, hobbies:'My hobbies are cooking, listening to music'},
-        {name:'Vijay', age:28},
-        {name:'Vinay', age:19 }
+      persons: [
+        { name: newName, age: 29, hobbies: 'My hobbies are cooking, listening to music' },
+        { name: 'Vijay', age: 28 },
+        { name: 'Vinay', age: 19 }
       ]
     });
     // this.state.persons[0].name='Udaya P Hugar'
   };
 
-  nameChangedHandler =(event)=>{
+  nameChangedHandler = (event) => {
     this.setState({
-      persons:[
-        {name:'Uday', age:29, hobbies:'My hobbies are cooking, listening to music'},
-        {name:event.target.value, age:28},
-        {name:'Vinay', age:19 }
+      persons: [
+        { name: 'Uday', age: 29, hobbies: 'My hobbies are cooking, listening to music' },
+        { name: event.target.value, age: 28 },
+        { name: 'Vinay', age: 19 }
       ],
-      showPersons:false
+      showPersons: false
     })
   }
 
-  togglePersons = ()=>{
+  togglePersons = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons : !doesShow}); 
+    this.setState({ showPersons: !doesShow });
   }
-  userNameChangeHandler =(event) =>{
+  userNameChangeHandler = (event) => {
     this.setState({
-      users:[
-        {username:event.target.value, email:'uday@test.com', name:'Uday'},
-        {username:'vijaykumar', email:'vijay@test.com', name:'Vijay'}
+      users: [
+        { username: event.target.value, email: 'uday@test.com', name: 'Uday' },
+        { username: 'vijaykumar', email: 'vijay@test.com', name: 'Vijay' }
       ]
     })
   }
   //passing method reference in the props
-  render() { 
-    const buttonStyle ={
-      backgroundColor:'yellow',
-      font:'inherit',
-      border:'1px solid red',
+  render() {
+    const buttonStyle = {
+      backgroundColor: 'yellow',
+      font: 'inherit',
+      border: '1px solid red',
       padding: '5px',
-      cursor:'pointer'
+      cursor: 'pointer'
     };
-    
-    return(
-    <div className="App">
-      <p>Hi, this is React app!</p>
-      {/* <UserInput usernameHandler={this.userNameChangeHandler} username={this.state.users[0].username}/>
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={() => this.switchNameHandler('Uday!')}
+          >{this.state.persons[0].hobbies}
+          </Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            nameChanged={this.nameChangedHandler}
+          >{this.state.persons[1].hobbies}
+          </Person>
+        </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        <p>Hi, this is React app!</p>
+        {/* <UserInput usernameHandler={this.userNameChangeHandler} username={this.state.users[0].username}/>
       <UserOutput name={this.state.users[0].name} email={this.state.users[0].email} username={this.state.users[0].username}/>
       <UserOutput name={this.state.users[1].name} email={this.state.users[1].email} username={this.state.users[1].username}/> */}
 
-      <button style={buttonStyle} onClick={this.togglePersons}>Toggle Persons</button>
-      {
-        this.state.showPersons ? 
-        <div>
-      <Person 
-        name={this.state.persons[0].name}
-        age={this.state.persons[0].age} 
-        click={() => this.switchNameHandler('Uday!')} 
-        >{this.state.persons[0].hobbies}
-      </Person>
-      <Person 
-        name={this.state.persons[1].name}
-        age={this.state.persons[1].age} 
-        nameChanged={this.nameChangedHandler}
-        >{this.state.persons[1].hobbies}
-      </Person>
-      </div> :null
-      }
-      
-      {/* <header className="App-header">
+        <button style={buttonStyle} onClick={this.togglePersons}>Toggle Persons</button>
+        {persons}
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> 
-      </header> */}
-    </div>
-    //react converts html lookalike jsx code to React.createElement. react takes care of the compilation. 
-    // React.createElement('div', null,'im react app!' )
-  )}
+      </div>
+      //react converts html lookalike jsx code to React.createElement. react takes care of the compilation. 
+      // React.createElement('div', null,'im react app!' )
+    )
+  }
 }
 
 export default App;
