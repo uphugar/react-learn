351c2a3e236288f51da961370614bb7b819b85ac
Panchakshari Hugar, Udaya <upanchaksharihuga@deloitte.com>
2020-04-19 13:48:34 +0530

    updating App as class Component to get started on learning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person' 
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    persons:[
+      {name:'Uday', age:29, hobbies:'My hobbies are cooking, listening to music'},
+      {name:'Vijay', age:28},
+      {name:'Vinay', age:19 }
+    ]
+  }
+  render() { return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>{this.state.persons[0].hobbies}</Person>
+      {/* <header className="App-header">
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> 
+      </header> */}
     </div>
-  );
+    //react converts html lookalike jsx code to React.createElement. react takes care of the compilation. 
+    // React.createElement('div', null,'im react app!' )
+  )};
 }
 
 export default App;
